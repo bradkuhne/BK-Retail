f5a461fc1e4f80adb215f21f6bf3c6b328148124
Brad Kuhne <bjkuhne@aol.com>
2022-02-19 17:41:00 -0600

    Categories find all GET working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,11 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  })
+  .then(data => res.json(data))
+  .catch(err => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
