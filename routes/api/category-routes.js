ed4195182c5ecffdfa7ddd2255be13069de8e0f6
Brad Kuhne <bjkuhne@aol.com>
2022-02-19 18:28:10 -0600

    Updated foreign keys between product and categories and working get categories by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,8 +12,15 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+  .then(console.log ("This is the id that was pass: " + req.params.id))
+  .then(data => res.json(data))
+  .catch(err => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
