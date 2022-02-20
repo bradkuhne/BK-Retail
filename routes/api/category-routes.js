929cb7135fdd0a8958b1cad8566ccb951d2a672e
Brad Kuhne <bjkuhne@aol.com>
2022-02-19 18:55:17 -0600

    Delete a category by id is working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,6 +45,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
